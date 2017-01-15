eee3fe5e264f21159f863a0a42f26f1c7e1c1f4d
Charles Wang <charles@thredup.com>
2017-01-14 16:49:17 -0800

    Styling just the wrappers to hold all the content

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,14 @@
+var Main = React.createClass({
+    render() {
+        return (
+            <div>
+                <div className="wrapper left">
+
+                </div>
+                <div className="wrapper right">
+
+                </div>
+            </div>
+        )
+    }
+});
